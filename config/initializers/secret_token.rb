f74ce1d55e51750910cfef17273fe3f41e6fc8b9
Richard Morgan <morganric@gmail.com>
2013-10-18 16:38:06 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Embedtree::Application.config.secret_token = '8a4e6b3ddb3afb87fb29d38af17fae45e5f81028af67c42694fd608dc45315203af22bdae888917f2edd063abc4adee4c297708a13830ffc778cfe28d251d6e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Embedtree::Application.config.session_store :cookie_store, key: '_embedtree_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Embedtree::Application.config.session_store :active_record_store
